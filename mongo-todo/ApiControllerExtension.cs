bb22d68c0c688e67bf5d41ac7cb194680634fe3b
Alexander Kahoun <alexkahoun@gmail.com>
2013-03-23 10:08:55 -0700

    Added new ApiControllerExtension

---
+++ mongo-todo/ApiControllerExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace mongo_todo
+{
+	public static class ApiControllerExtension
+	{
+		public static string GetLinkHeader<T>(this ApiController controller, HttpRequestMessage request, HttpResponseMessage response, string id, string relativeLocation)
+		{
+			return GetLinkHeader<T>(controller, request, response, id, relativeLocation, string.Empty);
+		}
+
+		public static string GetLinkHeader<T>(this ApiController controller, HttpRequestMessage request, HttpResponseMessage response, string relativeLocation)
+		{
+			return GetLinkHeader<T>(controller, request, response, string.Empty, relativeLocation, string.Empty);
+		}
+
+		public static string GetLinkHeader<T>(this ApiController controller, HttpRequestMessage request, HttpResponseMessage response, string id, string relativeLocation, string description)
+		{
+			string currentUri = controller.Request.RequestUri.AbsoluteUri;
+			if (!string.IsNullOrEmpty(id)) currentUri = string.Concat(currentUri, "/", id);
+
+			var uri = new Uri(string.Concat(currentUri, relativeLocation));
+			string rel = typeof(T).Name;
+
+			string type = string.Empty;
+			if (response != null
+				&& response.Content != null
+				&& response.Content.Headers != null
+				&& response.Content.Headers.ContentType != null
+			)
+				type = response.Content.Headers.ContentType.MediaType;
+
+			string location = string.Format("<{0}>", uri);
+			if (!string.IsNullOrWhiteSpace(rel)) string.Concat(location, "; rel=", rel);
+			if (!string.IsNullOrWhiteSpace(type)) string.Concat(location, "; type=\"", type, "\"");
+			if (!string.IsNullOrWhiteSpace(description)) string.Concat(location, "; title=\"", description, "\"");
+
+			return location;
+		}
+	}
+}
